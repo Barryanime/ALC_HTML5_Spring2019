d7645ee8a6725d20532a3f2dcd28a10c5a9f270e
Unknown <Barryanime123@gmail.com>
2019-01-23 10:28:09 -0700

    added index and game files

---
+++ Adventure Game/game.js
@@ -0,0 +1,19 @@
+// single line comment
+/*
+
+Multiline comment
+
+*/
+
+// document.write("<h1>I can't wait for <em>spring!</em></h1>");
+
+
+//alert("warning! Will Robinson...Warning!");
+
+//confirm("Do you like pokemon?");
+
+//prompt("What type of pokemon do you like?");
+ 
+var pc = prompt("What is your name");
+
+confirm("so your name is "+pc);
