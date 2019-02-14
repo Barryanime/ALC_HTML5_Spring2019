831e0f213fd6565d10e051eabbb44be1724d7aed
Unknown <Barryanime123@gmail.com>
2019-02-14 10:29:03 -0700

    added castle function

--- Adventure Game/game.js
+++ Adventure Game/game.js
@@ -48,26 +48,25 @@ function Game(){
 	
 	function Prison(){
 		var prison = prompt("You wake up...Your head is pounding and your vison is blurred. After pacing around walking off the headache your vision clears. It appears you are in a prison cell and you cannot remember why. \n -look around \n - go back to sleep \n - taunt the guard").toLowerCase();
-		
+        
 		if(prison == "look around" || prison == "look"){
-			
 			var prisonLook = prompt("The prison is a small rectagle shaped room, with stone walls and a dirt floor. A rat scurries into a hole in the corner. Bugs squirm and scamper underfoot. There is a barred window to the back. A bed occupied by a stranger asleep snoring to the right. The cell bars and locked door to the front. An empty bed which you had sleeped in the night before to the right.In the center of the dirt floor is a lumpy moldy damp rug. \n -wake stranger \n -move rug \n -eat bugs");
 		}
-		
-		else if(prison == "go back to sleep" || prison == "sleep"){
+        
+        		else if(prison == "go back to sleep" || prison == "sleep"){
 			alert("You fall back to sleep in your dirty cot. You dream of distant lands and adventures");
 			var resume = confirm("Do you wish to continue?");
 			
 			if(resume){
 				Prison();
 			}
+                    
 			else{
 				alert("Game Over!");
 			}
 		}
-		
 		else{
-			alert("I dont know what "+prison+" is!");
+			alert("I don't know what "+prison+" is!");
 			Prison();
 		}
 	}
@@ -152,6 +151,8 @@ function Game(){
                     
             }
         }
+    
+   
     
     
     
