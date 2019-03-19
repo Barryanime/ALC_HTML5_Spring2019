988210f9c7a3353ff38e9e8cbf2c38e4c79b3501
Unknown <Barryanime123@gmail.com>
2019-03-19 10:30:32 -0600

    CSS Excercise

--- Adventure Game/game.js
+++ Adventure Game/game.js
@@ -22,8 +22,8 @@ Multiline comment
     }
 
     var inventory = {
-        coins:1000,
-        bread:0,
+        coins:100,
+        bread:1,
         sword:0,
         map:0,
     }
@@ -36,13 +36,12 @@ Multiline comment
         
         return randInt;
     }
-
-// Start Game
+ 
 Game();
 
 function Game(){
     
-    var wizardNames = ["Bethazar", "Mordac", "Grendor", "Orco"];
+    var wizardNames = ["East Blue", "West BLue", "South Blue", "North Blue"];
 	
 	alert("Legend of "+wizardNames[Math.floor(Math.random()* 4)]+"!");
 	
@@ -52,19 +51,26 @@ function Game(){
         playerName = prompt("What name do you want?");
     }
 	
-	alert("Welcome to the land of Drizdal "+ playerName);
+	alert("Welcome to the land of One Piece "+ playerName);
 	
 	Prison();
 	
 	function Prison(){
-		var prison = prompt("You wake up...Your head is pounding and your vison is blurred. After pacing around walking off the headache your vision clears. It appears you are in a prison cell and you cannot remember why. \n -look around \n - go back to sleep \n - taunt the guard").toLowerCase();
+		var prison = prompt("You woke up and you realized you are in prison. \n -look around \n - go back to sleep").toLowerCase();
         
 		if(prison == "look around" || prison == "look"){
-			var prisonLook = prompt("The prison is a small rectagle shaped room, with stone walls and a dirt floor. A rat scurries into a hole in the corner. Bugs squirm and scamper underfoot. There is a barred window to the back. A bed occupied by a stranger asleep snoring to the right. The cell bars and locked door to the front. An empty bed which you had sleeped in the night before to the right.In the center of the dirt floor is a lumpy moldy damp rug. \n -wake stranger \n -move rug \n -eat bugs");
-		}
-        
+			var prisonLook = prompt("The prison is small with bugs squirming on the floor, there is a strange rug on the floor.  \n -move rug \n -eat bugs");
+            }
+        if(prisonLook == "move rug"  || prisonLook == "move"){
+                var prisonMove = alert("There's nothing beneath the rug");
+                var prisonLook = prompt("The prison is small with bugs squirming on the floor, there is a strange rug on the floor.  \n -move rug \n -eat bugs");
+            }
+        if(prisonLook == "eat bugs"  || prisonLook == "eat"){
+            var prisonEat = alert("The Bugs are poisonous! You died!")
+            
+        }
         		else if(prison == "go back to sleep" || prison == "sleep"){
-			alert("You fall back to sleep in your dirty cot. You dream of distant lands and adventures");
+			alert("You fell back to sleep in your bed.");
 			var resume = confirm("Do you wish to continue?");
 			
 			if(resume){
@@ -100,15 +106,15 @@ function Game(){
 	}
 	
 	function Swamp(){
-		var swampEnv = prompt("This is a dank swamp. \n -follow path \n -swim");
+		var swampEnv = prompt("You are in a swamp swamp. \n -follow path \n -swim");
 		
 		if(swampEnv == "follow" || swampEnv == "follow path"){
-			var swampPath = prompt("You enter on the swamp path and head north in the disance you see a swamp hut. As you approach you see a light burning inside. \n -enter hut \n -burn down hut");
+			var swampPath = prompt("You followed the path, heading north. You saw a swamp hut. \n -enter hut \n -burn down hut");
 			if( swampPath = "enter"){
-				alert("You entered the hut. There is an old hag bend over a black cauldron on the fire in the hearth");
+				alert("You entered the hut. There's nothing valuable inside");
 			}	
 			else if(swampPath = "burn down hut"){
-				alert("You burn down the hut...hope you feel good about yourself.. Pyro!");
+				alert("You burned down the hut along with yourself");
 			}	
 			else{
 				alert("dont throw rocks at hut...please!"); 
@@ -116,8 +122,8 @@ function Game(){
 			
 			
 		}
-		else if(swampEnv == "Swim"){
-			
+		else if(swampEnv == "swim"){
+			alert("You got eaten by Crocodiles")
 		}
 		else{
 			alert("I don't understand "+swampEnv); 
@@ -144,7 +150,7 @@ function Game(){
                             Blacksmith();
                         }
                         else{
-                        alert("Have a good day, come back again!");
+                        alert("Have a nice day!");
                             Blacksmith();
                         }
                 }
@@ -154,20 +160,20 @@ function Game(){
 		}
     }
         function Castle(){
-            var insideCastle = prompt("-upstairs -downstairs -courtyard -balcony -look").toLowerCase();
+            var insideCastle = prompt("-attic -downstairs -courtyard -balcony -look").toLowerCase();
             
             switch(insideCastle){
-                case "upstairs" || "go upstairs":
-                var upstairs = prompt("you head to to floor of castle");
+                case "attic" || "go to attic":
+                var upstairs = prompt("there's nothing in the attic");
                     
                 Castle();
                 break;
-                case "downstairs":
-                    alert("you go downstairs");
+                case "basement":
+                    alert("you went to the basement");
                     
                 break;
-                case "courtyard":
-                    alert("you go to the courtyard");
+                case "outside":
+                    alert("you went outside");
                     Castle();
                 break;
                 case "balcony":
